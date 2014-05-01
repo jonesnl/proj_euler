22922c167e557f694b5ca47fedc51fa3eb3db000
Nate Jones <jonesnl@umich.edu>
2014-04-30 20:30:16 -0400

    working on problem 3

---
+++ 3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long int large_int;
+
+large_int largest_factor(large_int a) {
+  large_int largest_factor = 1;
+  for (large_int i = 1; i < a / 2; ++i) {
+    if ()
+  }
+}
+
+int main() {
+  
+  
+
+  return 0;
+}
