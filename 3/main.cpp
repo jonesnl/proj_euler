d671d44c522189d2b4a0c698e13557c9c52e40a8
Nate Jones <jonesnl@umich.edu>
2014-05-01 01:06:45 -0400

    done problem 3

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 typedef long long int large_int;
 
 large_int largest_factor(large_int a) {
-  large_int largest_factor = 1;
-  for (large_int i = 1; i < a / 2; ++i) {
-    if ()
+  stack<large_int> nums_to_factor;
+  nums_to_factor.push(a);
+  large_int largest_factor = -1;
+
+  while (!nums_to_factor.empty()) {
+    large_int cur_factor = nums_to_factor.top();
+    nums_to_factor.pop();
+    large_int loop_bound = cur_factor / 2;
+
+    bool factored = false;
+    for (large_int i = 2; i < loop_bound; ++i) {
+      if (cur_factor % i == 0) {
+        factored = true;
+        if (i > largest_factor) largest_factor = i;
+        nums_to_factor.push(cur_factor / i);
+        break;
+      }
+    }
+
+    if (factored == false) {
+      if (cur_factor > largest_factor) largest_factor = cur_factor;
+    }
   }
+
+  return largest_factor;
 }
 
 int main() {
   
+
+  large_int l_factor = largest_factor(600851475143);
+
+  cout << l_factor << '\n';
   
 
   return 0;
